43aa695b0672d618e73e8b8545249aef7d9f5503
rnp39 <rnp39@cornell.edu>
2020-06-16 16:55:04 -0700

    experimenting with GADTs

--- simply-typed-lambda-calc/AST.hs
+++ simply-typed-lambda-calc/AST.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE ConstraintKinds   #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs             #-}
+
 module AST where
 
 type Id = String
@@ -41,7 +45,46 @@ data TBExpr =
 data TExpr =
   TENat TNExpr
   | TEBool TBExpr
-  | TCond TBExpr TNExpr TNExpr
+  | TCond TBExpr TExpr TExpr
   | TVar Id
   | TLam Id Type TExpr
   | TApp TExpr TExpr
+
+data Expr n b =
+  EXNat VNat
+  | EXBool VBool
+  | EXCond (b (Expr n b)) (Expr n b) (Expr n b)
+  | EXAdd (n (Expr n b)) (n (Expr n b))
+
+class Empty e
+instance Empty e
+
+data ENat = ENat VNat
+data EAdd a = EAdd a a
+
+class IsNatExpr a
+instance IsNatExpr ENat
+instance IsNatExpr a => IsNatExpr (EAdd a)
+
+data EBool = EBool VBool
+data EAnd a = EAnd a a
+
+class IsBoolExpr a
+instance IsBoolExpr EBool
+instance IsBoolExpr a => IsBoolExpr (EAnd a)
+
+data TyNat = TyNat
+data TyBool = TyBool
+
+data TyArrow t1 t2 = TyArrow t1 t2
+
+data GATExpr t n b where
+  GATNat :: VNat -> GATExpr TyNat n b
+  GATBool :: VBool -> GATExpr TyBool n b
+  GATAdd :: n e => e -> e -> GATExpr TyNat n b
+  GATAnd :: b e => e -> e -> GATExpr TyBool n b
+  GATCond :: b e1 => e1 -> e2 -> e2 -> GATExpr t n b
+
+type UGATExpr t = GATExpr t Empty Empty
+
+type TGATExpr t = GATExpr t IsNatExpr IsBoolExpr
