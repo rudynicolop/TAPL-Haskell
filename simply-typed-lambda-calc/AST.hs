ab1957dc96df206fc84c6ca383406024a3134b1a
rnp39 <rnp39@cornell.edu>
2020-06-16 11:05:30 -0700

    typed and untyped AST, need to factor out

--- simply-typed-lambda-calc/AST.hs
+++ simply-typed-lambda-calc/AST.hs
@@ -8,24 +8,40 @@ data VBool = VTrue | VFalse
 
 data VNat = Z | S VNat
 
-data NExpr =
+data UExpr =
+  UNat VNat
+  | UAdd UExpr UExpr
+  | USub UExpr UExpr
+  | UMul UExpr UExpr
+  | UBool VNat
+  | UNot UExpr
+  | UAnd UExpr UExpr
+  | UOr UExpr UExpr
+  | UEq UExpr UExpr
+  | ULeq UExpr UExpr
+  | UCond UExpr UExpr UExpr
+  | UVar Id
+  | ULam Id Type UExpr
+  | UApp UExpr UExpr
+
+data TNExpr =
   NNat VNat
-  | NAdd NExpr NExpr
-  | NSub NExpr NExpr
-  | NMul NExpr NExpr
+  | NAdd TNExpr TNExpr
+  | NSub TNExpr TNExpr
+  | NMul TNExpr TNExpr
 
-data BExpr =
+data TBExpr =
   BBool VBool
-  | BNot VBool
-  | BAnd VBool VBool
-  | BOr VBool VBool
-  | BEq NExpr NExpr
-  | BLeq NExpr NExpr
+  | BNot TBExpr
+  | BAnd TBExpr TBExpr
+  | BOr TBExpr TBExpr
+  | BEq TNExpr TNExpr
+  | BLeq TNExpr TNExpr
 
-data Expr =
-  ENat NExpr
-  | Bool BExpr
-  | ECond BExpr NExpr NExpr
-  | EVar Id
-  | ELam Id Type Expr
-  | EApp Expr Expr
+data TExpr =
+  TENat TNExpr
+  | TEBool TBExpr
+  | TCond TBExpr TNExpr TNExpr
+  | TVar Id
+  | TLam Id Type TExpr
+  | TApp TExpr TExpr
