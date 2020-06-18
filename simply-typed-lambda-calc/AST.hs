7da96019de30c16af8ec62112bce71347e3dc0ab
rnp39 <rnp39@cornell.edu>
2020-06-18 14:27:59 -0700

    type family experiment

--- simply-typed-lambda-calc/AST.hs
+++ simply-typed-lambda-calc/AST.hs
@@ -8,44 +8,68 @@
 {-# LANGUAGE RankNTypes                #-}
 {-# LANGUAGE ScopedTypeVariables       #-}
 {-# LANGUAGE StandaloneDeriving        #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 module AST where
 
 type Id = String
 
--- data Type = TNat | TBool | TArrow Type Type
-
 data VBool = VTrue | VFalse deriving (Eq)
 
 data VNat = Z | S VNat deriving (Eq)
 
-data EmptyArrow a b = EmptyArrow deriving (Eq)
+data NoType
+data NatType
+data BoolType
+data ArrowType a b = ArrowType a b
 
-data UnTyped = UnTyped deriving (Eq)
-data TyNat = TyNat deriving (Eq)
-data TyBool = TyBool deriving (Eq)
-data TyArrow t1 t2 = TyArrow t1 t2 deriving (Eq)
+type family Arrow t' t :: * where
+  Arrow NoType NoType = NoType
+  Arrow NatType NoType = NoType
+  Arrow BoolType NoType = NoType
+  Arrow (ArrowType t' t) NoType = NoType
+  Arrow a b = ArrowType a b
+
+-- c=constraint a=arrowConstructor n=natType b=boolType t=thisType
+data Expr c n b t where
+  XNat :: VNat -> Expr c n b n
+  XBool :: VBool -> Expr c n b b
+  XVar :: Id -> t -> Expr c n b t
+  XAdd :: Expr c n b n -> Expr c n b n -> Expr c n b n
+  XCond :: Expr c b n b -> Expr c b n t -> Expr c b n t -> Expr c b n t
+  XLam :: c t' => Id -> t' -> Expr c b n t -> Expr c b n (Arrow t' t)
+  XApp :: c t' => Expr c n b (Arrow t' t) -> Expr c n b t' -> Expr c n b t
 
 class Empty t
 instance Empty t
 
 class SimpType t
-instance SimpType TyNat
-instance SimpType TyBool
-instance (SimpType t1, SimpType t2) => SimpType (TyArrow t1 t2)
+instance SimpType NatType
+instance SimpType BoolType
+instance (SimpType t', SimpType t) => SimpType (ArrowType t' t)
 
-data Expr k n b t where
-  XNat :: VNat -> Expr k n b n
-  XBool :: VBool -> Expr k n b b
-  XVar :: Id -> Expr k n b t
-  XAdd :: Expr k n b n -> Expr k n b n -> Expr k n b n
-  XAnd :: Expr k n b b -> Expr k n b b -> Expr k n b b
-  XCond :: Expr k n b b -> Expr k n b t -> Expr k n b t -> Expr k n b t
-  XLam :: SimpType t' => Id -> t' -> Expr k n b t -> Expr k n b (k t' t)
-  XApp :: SimpType t' => Expr k n b (k t' t) -> Expr k n b t' -> Expr k n b t
+type UnTypedExpr = Expr Empty NoType NoType NoType
 
--- untyped programs
-type UExpr = Expr EmptyArrow UnTyped UnTyped UnTyped
-
--- typed programs
-type TExpr t = Expr TyArrow TyNat TyBool t
+-- data Type = TNat | TBool | TArrow Type Type
+--
+-- data UnTyped = UnTyped deriving (Eq)
+-- data TyNat = TyNat deriving (Eq)
+-- data TyBool = TyBool deriving (Eq)
+-- data TyArrow t1 t2 = TyArrow t1 t2 deriving (Eq)
+--
+--
+-- data Expr k n b t where
+--   XNat :: VNat -> Expr k n b n
+--   XBool :: VBool -> Expr k n b b
+--   XVar :: Id -> Expr k n b t
+--   XAdd :: Expr k n b n -> Expr k n b n -> Expr k n b n
+--   XAnd :: Expr k n b b -> Expr k n b b -> Expr k n b b
+--   XCond :: Expr k n b b -> Expr k n b t -> Expr k n b t -> Expr k n b t
+--   XLam :: SimpType t' => Id -> t' -> Expr k n b t -> Expr k n b (k t' t)
+--   XApp :: SimpType t' => Expr k n b (k t' t) -> Expr k n b t' -> Expr k n b t
+--
+-- -- untyped programs
+-- type UExpr = Expr EmptyArrow UnTyped UnTyped UnTyped
+--
+-- -- typed programs
+-- type TExpr t = Expr TyArrow TyNat TyBool t
--- simply-typed-lambda-calc/Checker.hs
+++ simply-typed-lambda-calc/Checker.hs
@@ -1,65 +1,140 @@
+-- {-# LANGUAGE DataKinds                 #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ExplicitForAll            #-}
 {-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE InstanceSigs              #-}
 {-# LANGUAGE RankNTypes                #-}
 
 module Checker where
-
-import           AST
-import           Data.Map.Strict as M
-
-data Type where
-  TNat :: TyNat -> Type
-  TBool :: TyBool -> Type
-  TArrow :: TyArrow Type Type -> Type
-  deriving (Eq)
-
-type Gamma = M.Map Id Type
-
-data TProg =
-  PNat (TExpr TyNat)
-  | PBool (TExpr TyBool)
-  | PArrow Type Type (TExpr (TyArrow Type Type))
-  -- deriving (Eq)
-
-type Result = Maybe TProg
-
-check :: Gamma -> UExpr -> Result
-check _ (XNat n)  = do return $ PNat $ XNat n
-check _ (XBool b) = do return $ PBool $ XBool b
-check g (XVar x)  = do
-  t <- M.lookup x g
-  case t of
-    TNat TyNat             -> do return $ PNat $ XVar x
-    TBool TyBool           -> do return $ PBool $ XVar x
-    TArrow (TyArrow t1 t2) -> do return $ PArrow t1 t2 $ XVar x
-check g (XAdd e1 e2) = do
-  e1' <- check g e1
-  e2' <- check g e2
-  case (e1', e2') of
-    (PNat p1, PNat p2) -> do return $ PNat $ XAdd p1 p2
-    _                  -> Nothing
-check g (XAnd e1 e2) = do
-  e1' <- check g e1
-  e2' <- check g e2
-  case (e1', e2') of
-    (PBool p1, PBool p2) -> do return $ PBool $ XAnd p1 p2
-    _                    -> Nothing
-check g (XCond e1 e2 e3) = do
-  e1' <- check g e1
-  e2' <- check g e2
-  e3' <- check g e3
-  checkcond e1' e2' e3'
-  where
-    checkcond :: TProg -> TProg -> TProg -> Result
-    checkcond  (PBool p1) (PBool p2) (PBool p3) = do return $ PBool $ XCond p1 p2 p3
-    checkcond  (PBool p1) (PNat p2) (PNat p3) = do return $ PNat $ XCond p1 p2 p3
-    checkcond  (PBool p1) (PArrow t21 t22 p2) (PArrow t31 t32 p3)
-      | t21 == t31 && t22 == t32 = do return $ PArrow t21 t22 $ XCond p1 p2 p3
-      | otherwise = Nothing
--- check g (XLam x t e) = do
---   e' <- check (M.insert x t g) e
---   case e' of
---     PBool p        -> do return $ PArrow t TyBool (XLam x t p)
---     PNat p         -> do return $ PArrow t TyNat (XLam x t p)
---     PArrow t1 t2 p -> do return $ PArrow t (TArrow t1 t2) (XLam x t p)
+--
+-- import           AST
+-- import           Data.Map.Strict as M
+--
+-- data Type where
+--   TNat :: TyNat -> Type
+--   TBool :: TyBool -> Type
+--   TArrow :: TyArrow Type Type -> Type
+--   deriving (Eq)
+--
+-- -- data TypeWrapper = TypeWrapper Type
+--
+-- -- * -> * -> *, where the last * is a Type
+--
+-- class TypeArrow a b where
+--   type TA = * -> * -> Type
+--
+-- instance TypeArrow a b where
+--   type TA a b = Type
+--
+-- -- wrapped types, toothless constraints
+-- type WExpr = Expr TypeArrow Type Type Type
+--
+-- type Gamma = M.Map Id Type
+--
+-- data TProg =
+--   PNat (TExpr TyNat)
+--   | PBool (TExpr TyBool)
+--   | PArrow Type Type (TExpr (TyArrow Type Type))
+--
+-- class WrapType t where
+--   wrapt :: t -> Type
+--
+-- instance WrapType TyNat where
+--   wrapt :: TyNat -> Type
+--   wrapt TyNat = TNat TyNat
+--
+-- instance WrapType TyBool where
+--   wrapt :: TyBool -> Type
+--   wrapt TyBool = TBool TyBool
+--
+-- instance (WrapType t1, WrapType t2) => WrapType (TyArrow t1 t2) where
+--   wrapt :: (TyArrow t1 t2) -> Type
+--   wrapt (TyArrow t1 t2) = TArrow $ TyArrow (wrapt t1) (wrapt t2)
+--
+-- class Wrap t where
+--   wrap :: (TExpr t) -> WExpr
+--
+-- instance Wrap Type where
+--   wrap :: (TExpr Type) -> WExpr
+--   wrap (XVar x)         = XVar x
+--   wrap (XCond e1 e2 e3) = XCond (wrap e1) (wrap e2) (wrap e2)
+--   wrap (XLam x t e)     = XLam x (wrapt t) (wrap e)
+--
+-- instance Wrap TyNat where
+--   wrap :: TExpr TyNat -> WExpr
+--   wrap (XNat n)         = XNat n
+--   wrap (XVar x)         = XVar x
+--   wrap (XAdd e1 e2)     = XAdd (wrap e1) (wrap e2)
+--   wrap (XCond e1 e2 e3) = XCond (wrap e1) (wrap e2) (wrap e3)
+--
+-- instance Wrap TyBool where
+--   wrap :: TExpr TyBool -> WExpr
+--   wrap (XBool b)        = XBool b
+--   wrap (XVar x)         = XVar x
+--   wrap (XAnd e1 e2)     = XAnd (wrap e1) (wrap e2)
+--   wrap (XCond e1 e2 e3) = XCond (wrap e1) (wrap e2) (wrap e3)
+--
+-- instance (Wrap t1, Wrap t2) => Wrap (TyArrow t1 t2) where
+--   wrap :: TExpr (TyArrow t1 t2) -> WExpr
+--   wrap (XVar x)         = (XVar x)
+--   wrap (XCond e1 e2 e3) = XCond (wrap e1) (wrap e2) (wrap e3)
+--   wrap (XLam x TyNat e) = XLam x (TNat TyNat) (wrap e)
+--
+-- wrapNatExpr :: TExpr TyNat -> WExpr
+-- wrapNatExpr (XNat n)     = XNat n
+-- wrapNatExpr (XVar x)     = XVar x
+-- wrapNatExpr (XAdd e1 e2) = XAdd (wrapNatExpr e1) (wrapNatExpr e2)
+-- wrapNatExpr (XCond e1 e2 e3) = XCond (wrapBoolExpr e1) (wrapNatExpr e2) (wrapNatExpr e3)
+--
+-- wrapBoolExpr :: TExpr TyBool -> WExpr
+-- wrapBoolExpr (XBool b)    = XBool b
+-- wrapBoolExpr (XVar x) = XVar x
+-- wrapBoolExpr (XAnd e1 e2) = XAnd (wrapBoolExpr e1) (wrapBoolExpr e2)
+-- wrapBoolExpr (XCond e1 e2 e3) = XCond (wrapBoolExpr e1) (wrapBoolExpr e2) (wrapBoolExpr e3)
+--
+-- wrapArrowExpr :: TExpr (TyArrow a b) -> WExpr
+-- wrapArrowExpr (XVar x) = XVar x
+-- wrapArrowExpr (XCond e1 e2 e3) = XCond (wrapBoolExpr e1) (wrapArrowExpr e2) (wrapArrowExpr e3)
+-- -- wrapArrowExpr (XLam x TyNat e) = XLam x (TNat TyNat) e
+--
+-- type Result = Maybe TProg
+--
+-- check :: Gamma -> UExpr -> Result
+-- check _ (XNat n)  = do return $ PNat $ XNat n
+-- check _ (XBool b) = do return $ PBool $ XBool b
+-- check g (XVar x)  = do
+--   t <- M.lookup x g
+--   case t of
+--     TNat TyNat             -> do return $ PNat $ XVar x
+--     TBool TyBool           -> do return $ PBool $ XVar x
+--     TArrow (TyArrow t1 t2) -> do return $ PArrow t1 t2 $ XVar x
+-- check g (XAdd e1 e2) = do
+--   e1' <- check g e1
+--   e2' <- check g e2
+--   case (e1', e2') of
+--     (PNat p1, PNat p2) -> do return $ PNat $ XAdd p1 p2
+--     _                  -> Nothing
+-- check g (XAnd e1 e2) = do
+--   e1' <- check g e1
+--   e2' <- check g e2
+--   case (e1', e2') of
+--     (PBool p1, PBool p2) -> do return $ PBool $ XAnd p1 p2
+--     _                    -> Nothing
+-- check g (XCond e1 e2 e3) = do
+--   e1' <- check g e1
+--   e2' <- check g e2
+--   e3' <- check g e3
+--   checkcond e1' e2' e3'
+--   where
+--     checkcond :: TProg -> TProg -> TProg -> Result
+--     checkcond  (PBool p1) (PBool p2) (PBool p3) = do return $ PBool $ XCond p1 p2 p3
+--     checkcond  (PBool p1) (PNat p2) (PNat p3) = do return $ PNat $ XCond p1 p2 p3
+--     checkcond  (PBool p1) (PArrow t21 t22 p2) (PArrow t31 t32 p3)
+--       | t21 == t31 && t22 == t32 = do return $ PArrow t21 t22 $ XCond p1 p2 p3
+--       | otherwise = Nothing
+-- -- check g (XLam x t e) = do
+-- --   e' <- check (M.insert x t g) e
+-- --   case e' of
+-- --     PBool p        -> do return $ PArrow t (TBool TyBool) (XLam x t p)
+-- --     PNat p         -> do return $ PArrow t (TNat TyNat) (XLam x t p)
+-- --     PArrow t1 t2 p -> do return $ PArrow t (TArrow $ TyArrow t1 t2) (XLam x t p)
