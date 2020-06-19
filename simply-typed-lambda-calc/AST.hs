69b60970b1cb9ff4d6376bd7a9de69ce584a80c0
rnp39 <rnp39@cornell.edu>
2020-06-18 18:08:53 -0700

    gave up on gadts...for now

--- simply-typed-lambda-calc/AST.hs
+++ simply-typed-lambda-calc/AST.hs
@@ -1,62 +1,36 @@
-{-# LANGUAGE ConstraintKinds           #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE ExplicitForAll            #-}
-{-# LANGUAGE FlexibleContexts          #-}
-{-# LANGUAGE FlexibleInstances         #-}
-{-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE MultiParamTypeClasses     #-}
-{-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE StandaloneDeriving        #-}
-{-# LANGUAGE TypeFamilies              #-}
-
 module AST where
 
 type Id = String
 
-data VBool = VTrue | VFalse deriving (Eq)
-
 data VNat = Z | S VNat deriving (Eq)
 
-data NoType = NoType
-  deriving (Eq)
+data VBool = VTrue | VFalse deriving (Eq)
 
-data NatType = NatType
-  deriving (Eq)
+data Type =
+  TNat
+  | TBool
+  | TArrow Type Type
 
-data BoolType = BoolType
-  deriving (Eq)
+data Expr t =
+  ENat VNat
+  | EBool VBool
+  | EVar (t Id)
+  | EAdd (t (Expr t)) (t (Expr t))
+  | EMul (t (Expr t)) (t (Expr t))
+  | ESub (t (Expr t)) (t (Expr t))
+  | EEq (t (Expr t)) (t (Expr t))
+  | ELeq (t (Expr t)) (t (Expr t))
+  | ENot (t (Expr t))
+  | EAnd (t (Expr t)) (t (Expr t))
+  | EOr (t (Expr t)) (t (Expr t))
+  | ECond (t (Expr t)) (t (Expr t)) (t (Expr t))
+  | ELam Id Type (t (Expr t))
+  | EApp (t (Expr t)) (t (Expr t))
 
-data ArrowType a b = ArrowType a b
-  deriving (Eq)
+data Bare e = Bare e
 
-type family Arrow t' t :: * where
-  Arrow NoType NoType = NoType
-  Arrow NatType NoType = NoType
-  Arrow BoolType NoType = NoType
-  Arrow (ArrowType t' t) NoType = NoType
-  Arrow a b = ArrowType a b
+data Typed e = Typed Type e
 
--- c=constraint a=arrowConstructor n=natType b=boolType t=thisType
-data Expr c n b t where
-  XNat :: VNat -> Expr c n b n
-  XBool :: VBool -> Expr c n b b
-  XVar :: Id -> Expr c n b t
-  XAdd :: Expr c n b n -> Expr c n b n -> Expr c n b n
-  XCond :: Expr c b n b -> Expr c b n t -> Expr c b n t -> Expr c b n t
-  XLam :: c t' => Id -> t' -> Expr c b n t -> Expr c b n (Arrow t' t)
-  XApp :: c t' => Expr c n b (Arrow t' t) -> Expr c n b t' -> Expr c n b t
+type BareExpr = Expr Bare
 
-class Empty t
-instance Empty t
-
-class SimpType t
-instance SimpType NatType
-instance SimpType BoolType
-instance (SimpType t', SimpType t) => SimpType (ArrowType t' t)
-
--- untyped expressions
-type UnTypedExpr = Expr Empty NoType NoType NoType
-
--- typed expressions
-type TypedExpr t = Expr SimpType NatType BoolType t
+type TypedExpr = Expr Typed
---
+++ simply-typed-lambda-calc/Hypothetical.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExplicitForAll            #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE StandaloneDeriving        #-}
+{-# LANGUAGE TypeFamilies              #-}
+
+module Hypothetical where
+
+import           AST
+
+{-Below is a failed experiment into Haskell's type system.
+
+  Hence this a "hypothetical" expression type for the
+  simply-typed lambda calculus. The idea, that the types in the
+  simply-typed lambda calculus are reflected in Haskell's type
+  system was interesting to pursue, but a type-checker for it would have
+  been infinite, because there are countably-infinite many types.
+
+  We begin with the base types, and construct more types with "->",
+  where the subsuquent layers are permutations of types defined in
+  previous layers.
+  -----------------------------------
+    Nat
+  -----------------------------------
+    Bool
+  -----------------------------------
+    Nat -> Nat
+    Nat -> Bool
+    Bool -> Nat
+    Bool -> Bool
+  -----------------------------------
+    Nat -> (Nat -> Nat)
+    Nat -> (Nat -> Bool)
+    Nat -> (Bool -> Nat)
+    Nat -> (Bool -> Bool)
+    Bool -> (Nat -> Nat)
+    Bool -> (Nat -> Bool)
+    Bool -> (Bool -> Nat)
+    Bool -> (Bool -> Bool)
+    (Nat -> Nat) -> Nat
+    (Nat -> Bool) -> Nat
+    (Bool -> Nat) -> Nat
+    (Bool -> Bool) -> Nat
+    (Nat -> Nat) -> Bool
+    (Nat -> Bool) -> Bool
+    (Bool -> Nat) -> Bool
+    (Bool -> Bool) -> Bool
+    (Nat -> Nat) -> (Nat -> Nat)
+    (Nat -> Nat) -> (Nat -> Bool)
+    (Nat -> Nat) -> (Bool -> Nat)
+    (Nat -> Nat) -> (Bool -> Bool)
+    (Nat -> Bool) -> (Nat -> Nat)
+    (Nat -> Bool) -> (Nat -> Bool)
+    (Nat -> Bool) -> (Bool -> Nat)
+    (Nat -> Bool) -> (Bool -> Bool)
+    (Bool -> Nat) -> (Nat -> Nat)
+    (Bool -> Nat) -> (Nat -> Bool)
+    (Bool -> Nat) -> (Bool -> Nat)
+    (Bool -> Nat) -> (Bool -> Bool)
+    (Bool -> Bool) -> (Nat -> Nat)
+    (Bool -> Bool) -> (Nat -> Bool)
+    (Bool -> Bool) -> (Bool -> Nat)
+    (Bool -> Bool) -> (Bool -> Bool)
+  -----------------------------------
+    ...
+  -----------------------------------
+  This process stops until a fixpoint of the type-space is reached,
+  but this is an infinite process.
+
+  c=constraint a=arrowConstructor n=natType b=boolType t=thisType -}
+
+type family Arrow t' t where
+  Arrow NoType NoType = NoType
+  Arrow NatType NoType = NoType
+  Arrow BoolType NoType = NoType
+  Arrow (ArrowType t' t) NoType = NoType
+  Arrow Type Type = Type
+  Arrow a b = ArrowType a b
+
+class SimpType t
+instance SimpType NatType
+instance SimpType BoolType
+instance (SimpType t', SimpType t) => SimpType (ArrowType t' t)
+
+data NoType = NoType
+  deriving (Eq)
+
+data NatType = NatType
+  deriving (Eq)
+
+data BoolType = BoolType
+  deriving (Eq)
+
+data ArrowType a b = ArrowType a b
+  deriving (Eq)
+
+data HyExpr c n b t where
+  XNat :: VNat -> HyExpr c n b n
+  XBool :: VBool -> HyExpr c n b b
+  XVar :: Id -> HyExpr c n b t
+  XAdd :: HyExpr c n b n -> HyExpr c n b n -> HyExpr c n b n
+  XCond :: HyExpr c n b b -> HyExpr c n b t -> HyExpr c n b t -> HyExpr c n b t
+  XLam :: SimpType t' => Id -> t' -> HyExpr c n b t -> HyExpr c n b (Arrow t' t)
+  XApp :: c t' => HyExpr c n b (Arrow t' t) -> HyExpr c n b t' -> HyExpr c n b t
+
+class Empty t
+instance Empty t
+
+-- hypothetical untyped expression type
+type HyUnTypedExpr = HyExpr Empty NoType NoType NoType
+
+-- hypothetical typed expressions
+type HyTypedExpr t = HyExpr SimpType NatType BoolType t
