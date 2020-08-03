0943a24760fd953c54055b56c5f0b479dd781923
rnp39 <rnp39@cornell.edu>
2020-08-03 13:36:35 -0700

    :can pass in files...yay?

--- mini-extended-stlc/Main.hs
+++ mini-extended-stlc/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
 module Main where
 
 import qualified AST                as A
@@ -8,13 +10,16 @@ import qualified Parser             as P
 import           System.Environment as OS
 
 main = do
-  [source] <- OS.getArgs
-  pgm <- readFile source
-  let ast = P.parse $ L.alexLex pgm
-  putStrLn $ show ast
-  case C.check M.empty ast of
-    Left msg           -> do putStrLn $ "Type-Error: " ++ msg
-    Right (C.R t tast) -> do
-      putStrLn "Successfully Type-checks! :D"
-      putStrLn $ "Program type: " ++ show t
-      putStrLn $ show tast
+  args <- OS.getArgs
+  case args of
+    [] -> putStrLn "Failed to pass in an stlc program as an argument"
+    source:_ -> do
+      pgm <- readFile source
+      let ast = P.parse $ L.alexLex pgm
+      putStrLn $ show ast
+      case C.check M.empty ast of
+        Left msg           -> do putStrLn $ "Type-Error: " ++ msg
+        Right (C.R t tast) -> do
+          putStrLn "Successfully Type-checks! :D"
+          putStrLn $ "Program type: " ++ show t
+          putStrLn $ show tast
